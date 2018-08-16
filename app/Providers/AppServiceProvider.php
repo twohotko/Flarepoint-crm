11a36d2703b215568e94dad31899b1675889d6ec
gptan <twohotko@gmail.com>
2018-08-16 14:30:38 +0800

    #Force Https on Production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Dusk\DuskServiceProvider;
 
@@ -10,10 +11,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      *
+     * @param UrlGenerator $url
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
+        if($this->app->environment('prod', 'production')) {
+            $url->forceScheme('https');
+        }
     }
 
     /**
